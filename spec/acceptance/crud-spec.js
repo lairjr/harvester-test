24b8a83e597e92fda3c151f3096eb04cfd21a0a9
Juraci Vieira <juraci.vieira@gmail.com>
2017-05-05 11:03:12 -0300

    passing POST and GET operations

--- spec/acceptance/crud-spec.js
+++ spec/acceptance/crud-spec.js
@@ -2,41 +2,57 @@ const harvesterApp = require('../../app/api');
 
 describe('CRUD operations', () => {
   let app;
+  const authorSchema = Joi.object().keys({
+    authors: Joi.array().items(Joi.object().keys({
+      id: Joi.string(),
+      name: Joi.string()
+    })).required()
+  });
+  const author = {
+    'authors':
+    [
+      {
+        'name': 'Stephen King'
+      }
+    ]
+  };
 
   beforeEach((done) => {
     app = harvesterApp.listen(3000, () => done());
   });
 
   afterEach(() => {
-    return server = null;
+    app.close()
   });
 
   describe('POST /authors', () => {
     beforeEach(() => app.adapter.db.models.author.remove({}));
 
     it('creates an author', (done) => {
-      const author = {
-        'authors':
-        [
-          {
-            'name': 'Stephen King'
-          }
-        ]
-      };
-
-      const schema = Joi.object().keys({
-        authors: Joi.array().items(Joi.object().keys({
-          id: Joi.string(),
-          name: Joi.string()
-        })).required()
-      });
-
       request
         .post('http://localhost:3000/authors')
         .set('Content-Type', 'application/json')
         .send(author)
         .end((err, res) => {
           expect(res.status).to.be.equal(201);
+          expect(res.body.authors[0].name).to.be.equal('Stephen King');
+          const result = Joi.validate(res.body, authorSchema);
+          expect(result.error).to.be.null
+          done(err);
+        });
+    });
+  });
+
+  describe('GET /authors', () => {
+    beforeEach(() => app.adapter.db.models.author.remove({})
+      .then(() => app.adapter.create('author', author)));
+
+    it('returns a collection of authors', () => {
+      request
+        .get('http://localhost:3000/authors')
+        .set('Content-Type', 'application/json')
+        .end((err, res) => {
+          expect(res.status).to.be.equal(200);
           expect(res.body.authors[0].name).to.be.equal('Stephen King');
           const result = Joi.validate(res.body, schema);
           expect(result.error).to.be.null
